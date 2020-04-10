278ec82a9af60b27369c775421b659eefb036219
K547NK <nigel.agwah@gmail.com>
2020-04-10 16:04:37 -0500

    housekeeping and updates

--- setup.sh
+++ setup.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
+#create and a local user config folder and copy all settings to it
+mkdir /home/nigel/.config && cp .config/* /home/nigel/.config/
+
+
 apt install vim neofetch i3blocks aptitude font-manager sudo gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 pip3 install powerline-shell
+
+#copy essential dotfiles to home folder
+cp .vimrc /home/nigel/ && cp .Xdefaults /home/nigel/ && cp .zshrc /home/nigel/
+
 sudo adduser nigel sudo
+
+#remove those annoying preinstalled tiny text editors
 apt remove nano vim-tiny
+
+#i hate to but i gots to install spotify for the music
 curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 
 apt-get update && apt-get install spotify-client -y
 
+#time for our favorite window manager
 git clone https://github.com/resloved/i3.git
 cd i3
 
@@ -22,5 +38,6 @@ mkdir -p build && cd build/
 make
 make install
 
+cd ..
 rm -rf i3
 
