74189754e432bd5d24e85aa3e0b1576e8566b3ad
K547NK <nigel.agwah@gmail.com>
2020-04-10 16:16:37 -0500

    updated user manuals for software

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #create and a local user config folder and copy all settings to it
-mkdir /home/nigel/.config && cp .config/* /home/nigel/.config/
+cp -r Manuals /home/nigel/
 
+mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
 
+#installing our fave programs and i3 gaps dependencies
 apt install vim neofetch i3blocks aptitude font-manager sudo gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
 
+#cause we all love zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 pip3 install powerline-shell
 
 #copy essential dotfiles to home folder
-cp .vimrc /home/nigel/ && cp .Xdefaults /home/nigel/ && cp .zshrc /home/nigel/
+cp -r .vimrc /home/nigel/ && cp -r .Xdefaults /home/nigel/ && cp -r .zshrc /home/nigel/
 
 sudo adduser nigel sudo
 
