1b6d0df24f4c4066c98a147e655d025fedbe71ea
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-10 05:28:52 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt install gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp pavucontrols sddm compton python3-pip xorg i3lock feh telegram-desktop firefox rofi zsh curl 
+
+apt install firwamre-linux-nonfree
+
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+apt-get update && apt-get install spotify-client
+
+git clone https://github.com/resloved/i3.git
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+make install
