d806081cdbf108ce3011b55fc9dc22e7bfa87f66
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-11 00:54:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,39 +5,24 @@ cp -r Pictures /home/nigel/
 mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
 
 #installing our fave programs and i3 gaps dependencies
-apt install sudo xterm htop vim neofetch imagemagick i3blocks aptitude ardour font-manager  gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
+sudo apt install xterm htop vim neofetch imagemagick i3blocks aptitude ardour font-manager  gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
 
 #make a directory for our sddm user icon and set permissions for sddm
-mkdir -p /usr/share/sddm/faces && cp -r nigel.face.icon /usr/share/faces/
+sudo mkdir -p /usr/share/sddm/faces && sudo cp -r nigel.face.icon /usr/share/faces/
 
 #remove those annoying preinstalled tiny text editors
-apt remove nano vim-tiny
+sudo apt remove nano vim-tiny
 
 #i hate to but i gots to install spotify for the music
-curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+sudo echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-apt update && apt install spotify-client -y
+sudo apt update && sudo apt install spotify-client -y
 
 #create and a local user config folder and copy all settings to it
 cp -r Manuals /home/nigel/
 cp -r Pictures /home/nigel/
 mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
-
-#installing our fave programs and i3 gaps dependencies
-apt install sudo xterm htop vim neofetch imagemagick i3blocks aptitude ardour font-manager  gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
-
-#make a directory for our sddm user icon and set permissions for sddm
-mkdir -p /usr/share/sddm/faces && cp -r nigel.face.icon /usr/share/faces/
-
-#remove those annoying preinstalled tiny text editors
-apt remove nano vim-tiny
-
-#i hate to but i gots to install spotify for the music
-curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
-
-apt update && apt install spotify-client -y
 
 #time for our favorite window manager
 git clone https://github.com/resloved/i3.git
@@ -57,22 +42,17 @@ mkdir -p build && cd build/
 
 make
 
-make install
+sudo make install
 
 cd ..
 #cause we all love zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #of course we love to have but not use powerline
-pip3 install powerline-shell
+sudo pip3 install powerline-shell
 
 #copy essential dotfiles to home folder
 cp -r .vimrc /home/nigel/ && cp -r .Xresources /home/nigel/ && cp -r .zshrc /home/nigel/
-
-chmod -R 755 /home/nigel/.config
-
-#add sudo
-sudo adduser nigel sudo
 
 #verify everything is properly installed
 echo "finishing touches..."
