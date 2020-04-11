64057115ac3bc38df11000be9b83dafdb975aeae
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-11 01:09:11 -0500

    Updated to install spotify for debian based distros
    Please review both scripts before execution and do so at your own risk

--- setup.sh
+++ setup.sh
@@ -2,15 +2,16 @@
 #create and a local user config folder and copy all settings to it
 cp -r Manuals /home/nigel/
 cp -r Pictures /home/nigel/
-mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
+mkdir ~/.config && cp -r .config/* ~/.config/
 
 #installing our fave programs and i3 gaps dependencies
 sudo apt install xterm htop vim neofetch imagemagick i3blocks aptitude ardour font-manager  gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
 
-#make a directory for our sddm user icon and set permissions for sddm
+#make a directory for our sddm user icon and set permissions for sddm replace you
+#comment out this line you dont need it unless you want my face on your computer
 sudo mkdir -p /usr/share/sddm/faces && sudo cp -r nigel.face.icon /usr/share/faces/
 
-#remove those annoying preinstalled tiny text editors
+#remove those annoying preinstalled tiny text editors which are powerfull yet incomplete
 sudo apt remove nano vim-tiny
 
 #i hate to but i gots to install spotify for the music
@@ -19,10 +20,10 @@ sudo echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/ap
 
 sudo apt update && sudo apt install spotify-client -y
 
-#create and a local user config folder and copy all settings to it
-cp -r Manuals /home/nigel/
-cp -r Pictures /home/nigel/
-mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
+#this adds a complete guide to Python Development and GNU Troff please comment out unnecessary lines
+cp -r Manuals ~/
+cp -r Pictures ~/
+mkdir ~/.config && cp -r .config/* ~/.config/
 
 #time for our favorite window manager
 git clone https://github.com/resloved/i3.git
@@ -45,14 +46,16 @@ make
 sudo make install
 
 cd ..
-#cause we all love zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#cause we all love zsh tthis will install oh-my-zsh and remove the one it file it adds
+sh -c $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf ~/.zshrc
 
 #of course we love to have but not use powerline
+#the customiztions is all left to you
 sudo pip3 install powerline-shell
 
 #copy essential dotfiles to home folder
-cp -r .vimrc /home/nigel/ && cp -r .Xresources /home/nigel/ && cp -r .zshrc /home/nigel/
+cp -r .vimrc ~/ && cp -r .Xresources ~/ && cp -r .zshrc ~/
 
 #verify everything is properly installed
 echo "finishing touches..."
