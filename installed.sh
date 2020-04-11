3a3e6ff401dc49433fa1ebbe6b9ab779ab1495e8
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-11 02:19:32 -0500

    Update installed.sh

--- installed.sh
+++ installed.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-echo "You have installed" && dpkg --list | wc --lines && echo "Packages"
-echo "rebooting now"
+echo "You have installed" && dpkg --list | wc --lines && echo "Packages" &&
+echo "rebooting now" && cd && rm -rf dotfiles
 sudo reboot
