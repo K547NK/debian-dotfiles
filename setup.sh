3224b3e8ba273e68ab86efa2929c5fa3665f0572
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-10 19:47:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,26 @@ echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.
 
 apt update && apt install spotify-client -y
 
+#create and a local user config folder and copy all settings to it
+cp -r Manuals /home/nigel/
+cp -r Pictures /home/nigel/
+mkdir /home/nigel/.config && cp -r .config/* /home/nigel/.config/
+
+#installing our fave programs and i3 gaps dependencies
+apt install sudo xterm htop vim neofetch imagemagick i3blocks aptitude ardour font-manager  gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ranger tlp sddm compton python3-pip xorg i3lock feh telegram-desktop firefox-esr rofi zsh curl -y
+
+#make a directory for our sddm user icon and set permissions for sddm
+mkdir -p /usr/share/sddm/faces && cp -r nigel.face.icon /usr/share/faces/
+
+#remove those annoying preinstalled tiny text editors
+apt remove nano vim-tiny
+
+#i hate to but i gots to install spotify for the music
+curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+
+apt update && apt install spotify-client -y
+
 #time for our favorite window manager
 git clone https://github.com/resloved/i3.git
 
