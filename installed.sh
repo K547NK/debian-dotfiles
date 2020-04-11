a163871175bc04d9ac5e690f6d05468624cc98af
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-11 04:31:27 -0500

    Update installed.sh

--- installed.sh
+++ installed.sh
@@ -1,4 +1,8 @@
 #!bin/bash
 echo "You have installed" && dpkg --list | wc --lines && echo "Packages" &&
 echo "rebooting now" && cd && rm -rf dotfiles
-sudo reboot
+
+gpg --full-gen-key &&
+pass init nigel.agwah@gmail.com &&
+mw add &&
+
