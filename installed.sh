54b1501072174fd32f3a56d8e13e159cc156074b
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-12 05:53:29 -0500

    Update installed.sh

--- installed.sh
+++ installed.sh
@@ -1,9 +1,20 @@
 #!bin/bash
-echo "You have installed" && dpkg --list | wc --lines && echo "Packages" &&
-echo "rebooting now" && cd && rm -rf dotfiles
-
+#set up email client
 gpg --full-gen-key &&
 pass init nigel.agwah@gmail.com &&
 mw add &&
-reboot 
 
+#set up github
+echo "adding github account"&&
+git config --global user.email "nigel.agwah@gmail.com" &&
+git config--global user.name "K547NK" &&
+
+#echo installed packages
+echo "You have installed" &&
+dpkg --list | wc --lines &&
+echo "Packages" &&
+
+#say bye bye
+echo "rebooting now" &&
+cd &&
+rm -rf dotfiles
