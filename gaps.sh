2111baafd363996bc3437a1b0884ccf84fd8dad4
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-13 09:15:20 -0500

    Update and rename setup.sh to gaps.sh

--- gaps.sh
+++ gaps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#create and a local user config folder and copy all settings to it
 
-
-#installing our fave programs and i3 gaps dependencies
+#installing i3 gaps dependencies
 sudo apt install gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev  -y
 
 #time for our favorite window manager
@@ -26,13 +24,8 @@ make
 sudo make install
 
 cd ..
-#cause we all love zsh tthis will install oh-my-zsh and remove the one it file it adds
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#of course we love to have but not use powerline
-#the customiztions is all left to you
-sudo pip3 install powerline-shell
 
 git clone https://github.com/LukeSmithxyz/mutt-wizard
 cd mutt-wizard
 sudo make install
+cd ..
