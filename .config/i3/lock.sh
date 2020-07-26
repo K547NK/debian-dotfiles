522c6bffc8a23279644854cfd5078ffaa877d643
Nigel Agwah <nigel.agwah@gmail.com>
2020-07-26 08:00:27 -0500

    clean up

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,4 +1,4 @@
-#!bin/zsh
+#!/bin/sh
 scrot ~/.config/i3/lock.png &&
 convert ~/.config/i3/lock.png -blur 8x8 ~/.config/i3/lock.png &&
-i3lock -i ~/.config/i3/lock.png
+i3lock -i ~/.config/i3/lock.png & rm ~/.config/i3/lock.png
